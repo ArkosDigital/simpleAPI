b822081b7d7f3895a584e9f084dae1828f2c9ded
Judah Holanda <judahholanda7@gmail.com>
2020-04-01 16:56:35 -0300

    update databases

--- tests/integration/database.ts
+++ tests/integration/database.ts
@@ -1,17 +1,11 @@
 import { DatabaseInfo } from 'flexiblepersistence';
 
 const database = new DatabaseInfo({
-  uri: process.env.DATABASE_URL,
-  username: process.env.POSTGRES_USER || 'postgres',
+  database: 'postgres',
   host: process.env.POSTGRES_HOST || 'localhost',
-  database: process.env.POSTGRES_DATABASE || 'dbauth',
-  password: process.env.POSTGRES_PASSWORD || 'evox2019',
   port: process.env.POSTGRES_PORT || 5432,
-  ssl: process.env.POSTGRES_SSL
-    ? {
-        rejectUnauthorized: false,
-      }
-    : undefined,
+  username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'evox2019',
 });
 
 export default database;
--- tests/integration/eventDatabase.ts
+++ tests/integration/eventDatabase.ts
@@ -2,12 +2,9 @@ import { MongoDB, DatabaseInfo } from 'flexiblepersistence';
 
 const eventDatabase = new MongoDB(
   new DatabaseInfo({
-    uri: process.env.MONGO_URI,
-    database: process.env.MONGO_DATABASE || 'write',
+    database: 'write',
     host: process.env.MONGO_HOST || 'localhost',
     port: process.env.MONGO_PORT,
-    username: process.env.MONGO_USER,
-    password: process.env.MONGO_PASSWORD,
   })
 );
 
