9bfdc2e59d98992b96344c02297edd3201ad1eba
Judah Holanda <judahholanda7@gmail.com>
2020-04-23 11:26:17 -0300

    update ts-mixer

--- source/service/baseServiceDefault.ts
+++ source/service/baseServiceDefault.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Journaly } from 'journaly';
 import { settings } from 'ts-mixer';
+import { Handler } from 'flexiblepersistence';
 settings.initFunction = 'init';
 /* eslint-disable @typescript-eslint/ban-ts-ignore */
 // @ts-ignore
@@ -11,11 +12,17 @@ export default class BaseServiceDefault {
   // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
   // @ts-ignore
   protected element: string;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  protected handler: any;
-  protected init(handler, journaly: Journaly<any>): void {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+  // @ts-ignore
+  protected handler: Handler;
+
+  public constructor(handler: Handler, journaly: Journaly<any>) {
     this.handler = handler;
     this.journaly = journaly;
+  }
+  protected init(handler: Handler, journaly: Journaly<any>): void {
     this.element = this.constructor.name;
+    this.handler = handler;
+    this.journaly = journaly;
   }
 }
--- source/service/baseServiceStore.ts
+++ source/service/baseServiceStore.ts
@@ -2,7 +2,7 @@ import ServiceStoreAdapter from '../adapter/service/serviceStoreAdapter';
 import BaseServiceDefault from './baseServiceDefault';
 import ServiceSimpleModel from '../model/serviceSimpleModel';
 import ServiceModel from '../model/serviceModel';
-import { Event, Operation } from 'flexiblepersistence';
+import { Event, Operation, Handler } from 'flexiblepersistence';
 import { settings } from 'ts-mixer';
 import { Journaly } from 'journaly';
 settings.initFunction = 'init';
@@ -10,17 +10,22 @@ settings.initFunction = 'init';
 // @ts-ignore
 export default class BaseServiceStore extends BaseServiceDefault
   implements ServiceStoreAdapter {
-  protected init(handler, journaly: Journaly<any>): void {
+  protected init(handler: Handler, journaly: Journaly<any>): void {
     super.init(handler, journaly);
-    console.log('store:', this.element);
+    this.journaly = journaly;
+    this.handler = handler;
+    console.log('store MF:', this.element);
+    console.log('store MF:', this.handler);
     // eslint-disable-next-line @typescript-eslint/no-this-alias
     const _self = this;
+    // if (this.journaly) {
     console.log(this.element + '.' + 'store', _self.store);
-    // this.journaly.subscribe(this.element + '.' + 'store', _self.store);
-    // this.journaly.subscribe(
-    //   this.element + '.' + 'storeElement',
-    //   _self.storeElement
-    // );
+    this.journaly.subscribe(this.element + '.' + 'store', _self.store);
+    this.journaly.subscribe(
+      this.element + '.' + 'storeElement',
+      _self.storeElement
+    );
+    // }
   }
   // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
   // @ts-ignore
@@ -29,6 +34,10 @@ export default class BaseServiceStore extends BaseServiceDefault
   ): Promise<ServiceModel>;
 
   public async store(content: ServiceSimpleModel): Promise<ServiceModel> {
+    console.log('this:', this);
+    // console.log('this.handler:', this.handler);
+    console.log('super.handler:', super.handler);
+
     // Event Sourcing
     const event = await this.handler.addEvent(
       new Event({
