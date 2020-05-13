5488277135aec9a9ee07039c0f3571eded8ef5e9
Judah Holanda <judahholanda7@gmail.com>
2020-05-13 09:48:20 -0300

    force ignore

--- test/integration/testService.ts
+++ test/integration/testService.ts
@@ -1,4 +1,4 @@
-/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/ban-ts-ignore */
 import {
   BaseService,
   ServiceModel,
@@ -6,10 +6,12 @@ import {
 } from '../../source/index';
 export default class TestService extends BaseService {
   public async selectElementById(id: string): Promise<ServiceModel> {
+    // @ts-ignore
     return (await this.journaly.publish('TestDAO.selectById', id))[0];
   }
 
   public async selectAllElements(): Promise<Array<ServiceModel>> {
+    // @ts-ignore
     return (await this.journaly.publish('TestDAO.selectAll'))[0];
   }
 
@@ -17,6 +19,7 @@ export default class TestService extends BaseService {
     content: ServiceSimpleModel
   ): Promise<ServiceModel> {
     console.log('store:' + content);
+    // @ts-ignore
     return (await this.journaly.publish('TestDAO.store', content))[0];
   }
 
@@ -24,22 +27,27 @@ export default class TestService extends BaseService {
     id: string,
     content: ServiceSimpleModel
   ): Promise<ServiceModel> {
+    // @ts-ignore
     return (await this.journaly.publish('TestDAO.update', id, content))[0];
   }
 
   public async deleteElement(id: string): Promise<boolean> {
+    // @ts-ignore
     return (await this.journaly.publish('TestDAO.delete', id))[0];
   }
 
   public selectById(id: string): Promise<ServiceModel> {
+    // @ts-ignore
     return super.selectById(id) as Promise<ServiceModel>;
   }
 
   public selectAll(): Promise<Array<ServiceModel>> {
+    // @ts-ignore
     return super.selectAll() as Promise<Array<ServiceModel>>;
   }
 
   public store(content: ServiceSimpleModel): Promise<ServiceModel> {
+    // @ts-ignore
     return super.store(content) as Promise<ServiceModel>;
   }
 
@@ -47,6 +55,7 @@ export default class TestService extends BaseService {
     id: string,
     content: ServiceSimpleModel
   ): Promise<ServiceModel> {
+    // @ts-ignore
     return super.update(id, content) as Promise<ServiceModel>;
   }
 }
