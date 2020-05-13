b5bab8cb17e9e03b74f7ef69e7426911e6b8409e
Judah Holanda <judahholanda7@gmail.com>
2020-05-13 09:41:45 -0300

    add test and refactor adapters

--- source/service/baseService.ts
+++ source/service/baseService.ts
@@ -1,8 +1,8 @@
+/* eslint-disable @typescript-eslint/ban-ts-ignore */
 import { Mixin } from 'ts-mixer';
 import ServiceAdapter from '../adapter/service/serviceAdapter';
 import BaseServiceRestricted from './baseServiceRestricted';
 import BaseServiceReserved from './baseServiceReserved';
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore
 export default abstract class BaseService
   extends Mixin(BaseServiceRestricted, BaseServiceReserved)
