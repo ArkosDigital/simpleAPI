f7f86fa2fe6d85a50baec579f7beedfc9c8c652e
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2020-06-02 11:02:08 -0300

    Controler default extending Default

--- source/controller/baseControllerDefault.ts
+++ source/controller/baseControllerDefault.ts
@@ -1,10 +1,19 @@
 /* eslint-disable @typescript-eslint/ban-ts-ignore */
-export default class BaseControllerDefault {
+import Default from '../default/default';
+import { settings } from 'ts-mixer';
+settings.initFunction = 'init';
+export default class BaseControllerDefault extends Default {
   protected errorStatus: {
     [error: string]: number;
   } = { Error: 400, error: 403, TypeError: 403, RemoveError: 400 };
   // @ts-ignore
   protected abstract elements: string;
-  // @ts-ignore
-  protected abstract element: string;
+
+  protected nameService: string | undefined;
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  protected async service(method: string, ...args: any): Promise<any[]> {
+    if (!this.nameService) this.nameService = this.element.replace('Controller', 'Service');
+    return this.journaly.publish(this.nameService + '.' + method, ...args);
+  }
 }
